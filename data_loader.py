fb3a40e3d89f089fbbd615259bfbd73ceb1fb331
Rodrigo <rodrigopuertas3@gmail.com>
2026-06-08 08:22:04 -0500

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,217 @@
+# -*- coding:UTF-8 -*-
+# Author:   Tiny Snow
+# Project:  Extended Vision Mutation Priority Promoter Encoder Framework (Transformer)
+# Time:     2022.5.18
+
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import csv
+import random
+import math
+
+import config as cfg
+
+
+class VarPromoterDataset(Dataset):
+    '''
+    Variation-based Promoter Dataset
+    
+    Args:
+        bases               array of base promoter (one-hot encode)
+        vars                k-mer of variant subsequence
+        indices             index of variations
+        acts                promoter fluorescence intensity
+
+    Dataset item:
+        with act            ({'base': base, 'var': var, 'index': index}, act)
+        no act              {'base': base, 'var': var, 'index': index}
+    '''
+    def __init__(self, bases, vars, indices, acts = None):
+        super(Dataset, self).__init__()
+        self.bases = bases
+        self.vars = vars
+        self.indices = indices
+        self.acts = acts
+
+    def __len__(self):
+        return len(self.bases)
+
+    def __getitem__(self, idx):
+        base = self.bases[idx]
+        var = self.vars[idx]
+        index = self.indices[idx]
+        promoter = {
+            'base': np.array(base, dtype = np.float64), 
+            'var': np.array(var, dtype = np.float64), 
+            'index': np.array(index, dtype = np.float64)
+        }
+        if self.acts:
+            act = self.acts[idx]
+            return (promoter, act)
+        else:
+            return promoter
+
+
+def one_hot(index, length):
+    '''
+    return one-hot encoding, len(onehot) = length, onehot[index] = 1
+    if index = length, then return [0, ..., 0]
+    '''
+    onehot = [0] * length
+    if index != length:
+        onehot[index] = 1
+    return np.array(onehot)
+
+def onehot_encode(promoter):
+    '''
+    one-hot encoding for promoter of length `seq_len`
+    one-hot encode element: B, A, T, C, G
+    '''
+    vocab = {'B': 0, 'A': 1, 'G': 2, 'C': 3, 'T': 4}
+    vocab_size = len(vocab)
+    onehots = []
+    for i in range(cfg.seq_len):
+        onehot = []
+        if i < len(promoter):
+            onehots.append(one_hot(vocab[promoter[i]], vocab_size))
+        else:
+            onehots.append(one_hot(vocab_size, vocab_size))
+    return np.array(onehots)
+
+
+def var_subseq(mother_promoter, origin_promoter):
+    vocab = {'B': 0, 'A': 1, 'G': 2, 'C': 3, 'T': 4}
+    vocab_size = len(vocab)
+
+    vars, indices = [], []
+
+    # padding hasta seq_len
+    origin_promoter += 'B' * max(0, cfg.seq_len - len(origin_promoter))
+    mother_promoter += 'B' * max(0, cfg.seq_len - len(mother_promoter))
+
+    for i in range(cfg.seq_len):
+        if origin_promoter[i] != mother_promoter[i]:
+            mer = [
+                vocab[origin_promoter[i]] if 0 <= k < cfg.seq_len else vocab_size
+                for k in range(
+                    i - ((cfg.mer - 1) // 2),
+                    i + (cfg.mer - ((cfg.mer - 1) // 2))
+                )
+            ]
+            var = np.array([one_hot(v, vocab_size) for v in mer]).reshape(-1)
+            vars.append(var)
+            indices.append(i)
+
+    # 🔹 muestreo si hay demasiadas mutaciones
+    if len(vars) > cfg.num_var:
+        sel = np.random.choice(len(vars), cfg.num_var, replace=False)
+        vars = [vars[i] for i in sel]
+        indices = [indices[i] for i in sel]
+
+    # 🔹 padding si hay pocas
+    pad_var = np.tile(one_hot(vocab_size, vocab_size), cfg.mer)
+    while len(vars) < cfg.num_var:
+        vars.append(pad_var)
+        indices.append(-1)
+
+    # 🔒 SIEMPRE devuelve algo
+    return np.array(vars), np.array(indices)
+
+
+
+def load_data():
+    '''
+    Load train/val/test/predict data
+    '''
+    # load train/val data
+    wild_promoter = {}
+    synthetic_promoter = []
+    with open(cfg.wild_data_path, 'r') as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        for i, [ID, mopromoter, promoter, act] in enumerate(reader):
+            wild_promoter[mopromoter] = {'Mopromoter': mopromoter, 'promoter': promoter, 'act': math.log10(float(act))}
+    with open(cfg.synthetic_data_path, 'r') as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        for i, [ID, mopromoter, promoter, act] in enumerate(reader):
+            synthetic_promoter.append({'Mopromoter': mopromoter, 'promoter': promoter, 'act': math.log10(float(act))})
+
+    wilds = list(wild_promoter.values())
+    random.shuffle(wilds)
+    random.shuffle(synthetic_promoter)
+    
+    train_bases, train_vars, train_indices, train_acts  = [], [], [], []
+    test_bases, test_vars, test_indices, test_acts      = [], [], [], []
+
+    # [test, train, valid]
+    test_percent = int(len(synthetic_promoter) * cfg.test_ratio)
+
+    for p in wilds:
+        train_bases.append(onehot_encode(p['Mopromoter']))
+        var, index = var_subseq(p['Mopromoter'], p['promoter'])
+        train_vars.append(var)
+        train_indices.append(index)
+        train_acts.append(p['act'])
+
+    for p in synthetic_promoter[: test_percent]:
+        var, index = var_subseq(p['Mopromoter'], p['promoter'])
+        if not var is None:
+            test_bases.append(onehot_encode(p['Mopromoter']))
+            test_vars.append(var)
+            test_indices.append(index)
+            test_acts.append(p['act'])
+    for p in synthetic_promoter[test_percent: ]:
+        var, index = var_subseq(p['Mopromoter'], p['promoter'])
+        if not var is None:
+            train_bases.append(onehot_encode(p['Mopromoter']))
+            train_vars.append(var)
+            train_indices.append(index)
+            train_acts.append(p['act'])
+
+    train_set       = VarPromoterDataset(train_bases, train_vars, train_indices, train_acts)
+    train_loader    = DataLoader(train_set, batch_size = cfg.batch_size, shuffle = True, drop_last = False)
+    test_set        = VarPromoterDataset(test_bases, test_vars, test_indices, test_acts)
+    test_loader     = DataLoader(test_set, batch_size = 1, shuffle = False, drop_last = False)
+
+    # load predict data
+    if cfg.if_predict:
+        predict_promoter = []
+        with open(cfg.predict_data_path, 'r') as f:
+            reader = csv.reader(f)
+            next(reader, None)
+            for i, [ID, mopromoter, promoter] in enumerate(reader):
+                if mopromoter in wild_promoter:
+                    predict_promoter.append({'Mopromoter': mopromoter, 'promoter': promoter})
+                else:
+                    predict_promoter.append({'Mopromoter': mopromoter, 'promoter': promoter})
+        predict_bases, predict_vars, predict_indices = [], [], []
+        for p in predict_promoter:
+            var, index = var_subseq(p['Mopromoter'], p['promoter'])
+            if not var is None:
+                predict_bases.append(onehot_encode(p['Mopromoter']))
+                predict_vars.append(var)
+                predict_indices.append(index)
+        predict_set = VarPromoterDataset(predict_bases, predict_vars, predict_indices)
+        predict_loader = DataLoader(predict_set, batch_size = 1, shuffle = False, drop_last = False)
+    else:
+        predict_loader = None
+
+    return train_loader, test_loader, predict_loader
+
+
+def train_valid_spilt(train_loader, valid_ratio = cfg.val_ratio):
+    '''
+    split data into train and valid
+    '''
+    train_set = train_loader.dataset
+    train_size = len(train_set)
+    valid_size = int(train_size * valid_ratio)
+    train_size = train_size - valid_size
+    train_set, valid_set = torch.utils.data.random_split(train_set, [train_size, valid_size])
+    train_loader = DataLoader(train_set, batch_size = cfg.batch_size, shuffle = True, drop_last = False)
+    valid_loader = DataLoader(valid_set, batch_size = cfg.batch_size, shuffle = True, drop_last = False)
+    return train_loader, valid_loader
+
---
+++ helper.py
@@ -0,0 +1,55 @@
+# -*- coding:UTF-8 -*-
+# Author:   Tiny Snow
+# Project:  Extended Vision Mutation Priority Promoter Encoder Framework (Transformer)
+# Time:     2022.5.18
+
+import os
+import config as cfg
+import random
+import numpy as np
+import torch
+
+
+class Signal:
+    '''Running signal to control training process'''
+
+    def __init__(self, signal_file):
+        self.signal_file = signal_file
+        self.train = True
+        self.update()
+
+    def update(self):
+        signal_dict = self.read_signal()
+        self.train = signal_dict['train']
+
+    def read_signal(self):
+        with open(self.signal_file, 'r') as fin:
+            return eval(fin.read())
+
+
+def log(message):
+    '''Log --> log file'''
+    if os.path.exists(cfg.log_file):
+        with open(cfg.log_file, 'a') as f:
+            f.write(message + '\n')
+    else:
+        with open(cfg.log_file, 'w') as f:
+            f.write(message + '\n')
+    print(message)
+
+
+def create_dir(path):
+    '''Create directory if not exists'''
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def seed_all(seed = 2023):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    
